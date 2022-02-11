34e27e3b9bcbc1dcdf8f7c4d43487d38b9492efa
Daniela Navarro <daninava2009@hotmail.com>
2022-02-10 19:05:58 -0600

    Diseño de interface

---
+++ controlador.js
@@ -0,0 +1,48 @@
+import { convierteAPosfija } from "./posfija.js"; //Importa la función de conversión
+/**
+ * Obtiene los elementos HTML a manipular mediante código
+ *  - boton: boton html que desencadena las acciones del algoritmo
+ *            de conversión
+ *  - inputExpresion: input html donde el usuario ingresa la expresion
+ *                    a convertir
+ *  - inputResultado: input html donde el usuario visualiza el resultado
+ *                    de conversión de la expresión regular
+ *  - contenedorResultado: div html que agrupa al botón de conversión
+ *                          y los inputs
+ *  - contenedor: div html que agrupa todos los componentes del programa
+ */
+const boton = document.getElementById("boton-convertir");
+const inputExpresion = document.getElementById("expresion-regular");
+const inputResultado = document.getElementById("resultado-conversion");
+const contenedorResultado = document.getElementById("div-programa");
+const contenedor = document.getElementById("contenedor-principal");
+
+/**
+ * Crea un div html donde se desplegará el mensaje de
+ * error en caso de que este ocurra
+ */
+const alerta = document.createElement("div");
+
+/**
+ * Ejecuta la función de conversión de la expresión infija 
+ * a posfija.
+ * Despliega el resultado en el inputResultado en caso de que
+ * la operación se haya realizado correctamente.
+ * En caso contrario agrega el texto de error y las clases
+ * bootstrap al elemento alerta creado anteriormente y lo
+ * renderiza en el html
+ */
+// 3*(6-4+(2*3))+1
+const clickBotonConvierte = () => {
+  const expresion = inputExpresion.value;
+  try {
+    let posfija = convierteAPosfija(expresion);
+    inputResultado.value = posfija;
+  } catch (error) {
+    alerta.setAttribute("class", "alert alert-warning my-0");
+    alerta.textContent = error.mensaje + error.caracter;
+    contenedor.insertBefore(alerta, contenedorResultado);
+    setTimeout(() => contenedor.removeChild(alerta), 2000);
+  }
+};
+boton.addEventListener("click", clickBotonConvierte);
--- posfija.js
+++ posfija.js
@@ -1,4 +1,118 @@
+const operador = {
+  orden: 0,
+  simbolo: "",
+};
+const arregloOperadores = ["^", "*", "/", "+", "-"];
+let posfija = "";
 
-console.log('hola mundo');
+const definePrioridad = (arregloOps = []) => {
+  const resultado = arregloOps.map((elemento, indice) => {
+    if (elemento == "*" || elemento == "/") {
+      return inicializaOperador(2, elemento);
+    }
+    if (elemento == "+" || elemento == "-") {
+      return inicializaOperador(3, elemento);
+    } else {
+      return inicializaOperador(indice + 1, elemento);
+    }
+  });
+  return resultado;
+};
 
-console.log("Hola mundo 2");
+const inicializaOperador = (orden, simbolo) => {
+  const operadorSimbolo = { ...operador };
+  operadorSimbolo["orden"] = orden;
+  operadorSimbolo["simbolo"] = simbolo;
+  return operadorSimbolo;
+};
+
+const inicializaInfija = (expresion = "") => {
+  return expresion.split("");
+};
+
+const esOperador = (caracter = "") => {
+  for (let i = 0; i < arregloOperadores.length; i++) {
+    const operador = arregloOperadores[i];
+    if (caracter == operador) {
+      return caracter;
+    }
+  }
+  return false;
+};
+
+const esOperando = (caracter = "") => {
+  for (let i = 0; i < 10; i++) {
+    if (Number(caracter) == i) return caracter;
+  }
+  return false;
+};
+
+const parentesisDerecho = (pila = []) => {
+  for (let i = 0; i < pila.length; i++) {
+    const elemento = pila[i];
+    if (elemento != "(") {
+      posfija += pila.pop();
+      continue;
+    }
+    pila.pop();
+    break;
+  }
+};
+
+const caracterOperador = (pila = [], operador = "") => {
+  let bandera = true;
+  const tope = pila[pila.length - 1];
+  while (bandera) {
+    if (pila.length == 0 || tope == "(" || mayorPrioridad(operador, tope)) {
+      pila.push(operador);
+      bandera = false;
+    } else posfija += pila.pop();
+  }
+};
+
+const mayorPrioridad = (operador = "", tope = "") => {
+  const prioridadOperador = definePrioridad([operador]);
+  const prioridadTope = definePrioridad([tope]);
+  return prioridadOperador[0].orden < prioridadTope[0].orden;
+};
+
+function excepcionCaracter(caracter) {
+  this.mensaje = "Caracter no válido: ";
+  this.caracter = caracter;
+}
+
+const convierteInfija = (arregloInfija = [], pila = []) => {
+  posfija = "";
+  const prioridades = definePrioridad(arregloOperadores);
+  let indice = 0;
+  let caracter = arregloInfija[indice];
+
+  while (indice < arregloInfija.length) {
+    switch (caracter) {
+      case "(":
+        pila.push(caracter);
+        break;
+      case ")":
+        parentesisDerecho(pila);
+        break;
+      case esOperador(caracter):
+        caracterOperador(pila, caracter);
+        break;
+      case esOperando(caracter):
+        posfija += caracter;
+        break;
+      default:
+        throw new excepcionCaracter(caracter);
+        break;
+    }
+    caracter = arregloInfija[++indice];
+  }
+  pila.forEach((elemento) => (posfija += elemento));
+};
+
+export function convierteAPosfija(infija = "") {
+  const pila = [];
+  const arregloInfija = inicializaInfija(infija);
+  convierteInfija(arregloInfija, pila);
+  return posfija;
+}
