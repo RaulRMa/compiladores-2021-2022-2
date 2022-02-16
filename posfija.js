0491bd49bccd8cac6b5f201ca271c2121604770d
Raul Ruperto <raul.rupertojs@gmail.com>
2022-02-16 15:33:50 -0600

    Expresiones regulares

--- posfija.js
+++ posfija.js
@@ -2,16 +2,37 @@ const operador = {
   orden: 0,
   simbolo: "",
 };
-const arregloOperadores = ["^", "*", "/", "+", "-"];
+const arregloOperadores = ["|", "&", "+", "*", "?"];
 let posfija = "";
+
+const prioridadAlfabeto = () => {};
+
+/**
+ * Convierte el parámetro a código ascii y lo compara
+ * con el rango 65...90 y 97...122.
+ * @param {char} letra
+ * @returns caracter comparado con el código ascii, carácter nulo si no pertenece al alfabeto
+ */
+const esOperadorLetra = (letra = "") => {
+  const asciiLetra = letra.charCodeAt();
+  //Codigo ascii de letras mayúsculas
+  for (let i = 65; i <= 90; i++) {
+    if (i == asciiLetra) return letra;
+  }
+  //Codigo ascii de letras minúsculas
+  for (let j = 97; j <= 122; j++) {
+    if (j == asciiLetra) return letra;
+  }
+  return "\0";
+};
 
 const definePrioridad = (arregloOps = []) => {
   const resultado = arregloOps.map((elemento, indice) => {
-    if (elemento == "*" || elemento == "/") {
-      return inicializaOperador(2, elemento);
-    }
-    if (elemento == "+" || elemento == "-") {
+    if (elemento == "*" || elemento == "+"  || elemento == "?") {
       return inicializaOperador(3, elemento);
+    }
+    if (elemento == "&") {
+      return inicializaOperador(2, elemento);
     } else {
       return inicializaOperador(indice + 1, elemento);
     }
@@ -44,7 +65,7 @@ const esOperando = (caracter = "") => {
   for (let i = 0; i < 10; i++) {
     if (Number(caracter) == i) return caracter;
   }
-  return false;
+  return esOperadorLetra(caracter);
 };
 
 const parentesisDerecho = (pila = []) => {
@@ -110,9 +131,41 @@ const convierteInfija = (arregloInfija = [], pila = []) => {
   pila.forEach((elemento) => (posfija += elemento));
 };
 
-export function convierteAPosfija(infija = "") {
+const convierteAInfija = (expresion = '') =>{
+  const arreglo = expresion.split('');
+  let cadena = ''
+  for(let i = 0; i < arreglo.length; i++){
+    const caracter = arreglo[i];
+    
+    if(caracter != "]") cadena += caracter;
+    if(i + 1 < arreglo.length){
+      const indice = i + 1;
+      const actual = arreglo[indice]
+      const anterior = arreglo[indice - 1];
+      switch(actual){
+        case '|':
+        case '*':
+        case '+':
+        case '?':
+        case '&':
+        case ')': break;
+        default:
+          if(anterior != "(" && anterior != "|" && anterior != "&" && indice - 1 >= 0){
+            cadena += '&';
+          }
+          break;
+      }
+    }
+  }
+  return cadena;
+}
+
+export function convierteAPosfija(expReg = "") {
+  console.log("Entrando a la funcion");
   const pila = [];
-  const arregloInfija = inicializaInfija(infija);
-  convierteInfija(arregloInfija, pila);
+  //const arregloInfija = inicializaInfija(expReg);
+  const resultado = convierteAInfija(expReg);
+  console.log(resultado);
+  convierteInfija(resultado, pila);
   return posfija;
 }
