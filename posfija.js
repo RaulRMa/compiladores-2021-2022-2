95337502c478c89c320c3c9588a851bcb6da1515
Daniela Navarro <daninava2009@hotmail.com>
2022-02-17 15:27:29 -0600

    comentado y prueba

--- posfija.js
+++ posfija.js
@@ -1,12 +1,12 @@
+//DEfines un objeto de operador 
 const operador = {
   orden: 0,
   simbolo: "",
-  unario: false,
+  unario: false, //Valor boleano 
 };
 const arregloOperadores = ["|", "&", "+", "*", "?"];
 let posfija = "";
 
-const prioridadAlfabeto = () => {};
 
 /**
  * Convierte el parámetro a código ascii y lo compara
@@ -24,22 +24,24 @@ const esOperadorLetra = (letra = "") => {
   for (let j = 97; j <= 122; j++) {
     if (j == asciiLetra) return letra;
   }
+  //codigo ascii del punto S
+  if(asciiLetra == 46) return letra;
   return "\0";
 };
 
 //Orden de precedencia 
 const definePrioridad = (arregloOps = []) => {
   const resultado = arregloOps.map((elemento, indice) => {
-    //Operadores Unarios
+    //Operadores Unarios 3
     if (elemento == "*" || elemento == "+"  || elemento == "?") {
       return inicializaOperador(3, elemento, true);
     }
-    //Concatenacion
+    //Concatenacion 2
     if (elemento == "&") {
       return inicializaOperador(2, elemento, false);
     } else {
 
-      //Seleccion de alternativas 
+      //Seleccion de alternativas 1
       return inicializaOperador(indice + 1, elemento, false);
     }
   });
@@ -55,24 +57,17 @@ const inicializaOperador = (orden, simbolo, unario) => {
   return operadorSimbolo;
 };
 
+//checa si esta dentro del arreglo de operadores 
 const esOperador = (caracter = "") => {
   for (let i = 0; i < arregloOperadores.length; i++) {
     const operador = arregloOperadores[i];
-    if (caracter == operador) {
+    if (caracter == operador) { //en caso de que este, regresa el caracter  comparado
       return caracter;
     }
   }
   return false;
 };
 
-//Rango de operando sea de numero
-const esOperando = (caracter = "") => {
-  for (let i = 0; i < 10; i++) {
-    if (Number(caracter) == i) return caracter;
-  }
-  //si no el operador es letra 
-  return esOperadorLetra(caracter);
-};
 
 //Si el caracter es parentesis derecho 
 const parentesisDerecho = (pila = []) => {
@@ -112,21 +107,22 @@ const caracterOperador = (pila = [], operador = "") => {
   }
 };
 
+//Define las prioridades y el orden 
 const mayorPrioridad = (operador = "", tope = "") => {
   const prioridadOperador = definePrioridad([operador]);
   const prioridadTope = definePrioridad([tope]);
   return prioridadOperador[0].orden > prioridadTope[0].orden;
 };
 
+//Manda mensaje de error
 function excepcionCaracter(caracter) {
   this.mensaje = "Caracter no válido: ";
   this.caracter = caracter;
 }
-
+//Algoritmo Principal
 const convierteInfAPos = (arregloInfija = [], pila = []) => {
   posfija = "";
-  const prioridades = definePrioridad(arregloOperadores);
-  let indice = 0;
+  let indice = 0; //posicion primera del arreglo
   let caracter = arregloInfija[indice];
 
   while (indice < arregloInfija.length) {
@@ -140,12 +136,11 @@ const convierteInfAPos = (arregloInfija = [], pila = []) => {
       case esOperador(caracter):
         caracterOperador(pila, caracter);
         break;
-      case esOperando(caracter):
+      case esOperadorLetra(caracter):
         posfija += caracter;
         break;
       default:
         throw new excepcionCaracter(caracter);
-        break;
     }
     caracter = arregloInfija[++indice];
   }
@@ -158,28 +153,49 @@ const convierteAInfija = (expresion = "") => {
   for (let i = 0; i < arreglo.length; i++) {
     const caracter = arreglo[i];
 
-    if (caracter != "]") cadena += caracter;
-
-    if (i + 1 < arreglo.length) {
-      const indice = i + 1;
-      const actual = arreglo[indice];
-      const anterior = arreglo[indice - 1];
-      if (esOperador(actual) || actual == ")") continue;
-      if (
-        anterior != "(" &&
-        anterior != "|" &&
-        anterior != "&" &&
-        indice - 1 >= 0
-      )
-        cadena += "&";
+    //Detecta corchetes izquierdos
+    if(caracter == "["){
+      const siguiente = arreglo[i+1];
+      cadena += "(" + siguiente;
+      let acumulador = 0;
+      if(arreglo[i + 2] == '-'){
+        const inicio = siguiente.charCodeAt();
+        const final = arreglo[i+3].charCodeAt();
+        for(acumulador = inicio + 1; acumulador <= final; acumulador++) 
+          cadena += "|" + String.fromCharCode(acumulador);
+        i += 3;
+      }else{
+        for(acumulador = i +2; arreglo[acumulador] != ']'; acumulador++)
+          cadena += "|" + arreglo[acumulador];
+        i = acumulador -1;
+      }
+      cadena += ")"
+    }else{
+      if (caracter !== "]") cadena += caracter;
+  
+      if (i + 1 < arreglo.length) {
+        const indice = i + 1;
+        const actual = arreglo[indice];
+        const anterior = arreglo[indice - 1];
+        if (esOperador(actual) || actual == ")") continue;
+        if (
+          anterior != "(" &&
+          anterior != "|" &&
+          anterior != "&" &&
+          indice - 1 >= 0
+        )
+          cadena += "&";
+      }
     }
+
   }
   return cadena;
 };
 
 export function convierteAPosfija(expReg = "") {
+  const expresionRegular = expReg.trim();
   const pila = [];
-  const resultado = convierteAInfija(expReg);
+  const resultado = convierteAInfija(expresionRegular);
   convierteInfAPos(resultado, pila);
   return posfija;
 }
