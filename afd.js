0b3af63ab87e03a67c8a266e402771a449876ccc
Raul Ruperto <raul.rupertojs@gmail.com>
2022-03-16 15:56:53 -0600

    Tabla afd

--- afd.js
+++ afd.js
@@ -10,7 +10,6 @@ Array.prototype.equals = function (getArray) {
     }
     return true;
 };//Sobre escribe el método de javascript para comparar 2 arreglos
-
 //const dEstados = [];
 const dEstado = (nombre = '', subconjuntos = []) =>{
     return {
@@ -30,16 +29,27 @@ const cerraduraEpsilon = (transiciones = [], encabezados = []) => {
     recorridoEpsilon(edoInicio,transiciones,edoA);
     dEstados.push(dEstado("A",edoA));
     const conjuntos = [];
+    const TABLA = [];
     for(let k = 0; k < dEstados.length; k++){
         const dEstadoSub = dEstados[k];
+        const fila = {
+            estado: "",
+            encabezados:[],
+            transiciones: [],
+        };
+        fila.estado = dEstadoSub.nombre;
         for (let c = 0; c < encs.length; c++) {
             const ultimo = dEstados[dEstados.length -1];
             const encabezado = encs[c];
+            fila.encabezados.push(encabezado);
             let estados = [];
             dEstadoSub.subconjuntos.forEach(edo => {
                 estadoEncabezado(transiciones,encabezado,estados,edo);
             })
-            if(estados.length == 0) continue;
+            if(estados.length == 0){
+                fila.transiciones.push("");
+                continue;
+            }
             marcaNovisitados(transiciones);
             const subconjuntos = [];
             let nombreCerradura = '{';
@@ -49,15 +59,20 @@ const cerraduraEpsilon = (transiciones = [], encabezados = []) => {
                 subconjuntos.push({estado:edo});
                 recorridoEpsilon(edo,transiciones,subconjuntos);
             });
+            objeto.subconjuntos = subconjuntos;
             if(!subConjuntoExiste(dEstados,subconjuntos)){
-                objeto.subconjuntos = subconjuntos;
                 dEstados.push(objeto);
                 conjuntos.push(subconjuntos);
+            }else{
+                const nombreEstado = subConjuntoRepetido(dEstados,subconjuntos);
+                fila.transiciones.push(nombreEstado.nombre);
             }
             
         }
+        TABLA.push(fila);
     }
-    console.log(dEstados);
+    console.log(TABLA);
+    return dEstados;
 }
 
 const subConjuntoExiste = (dEstados = [], subconj = []) =>{
@@ -67,7 +82,14 @@ const subConjuntoExiste = (dEstados = [], subconj = []) =>{
     }
     return false;
 }
-
+const subConjuntoRepetido = (dEstados = [], subconj = []) =>{
+    for (let i = 0; i < dEstados.length; i++) {
+        const subconjunto = dEstados[i].subconjuntos;
+        if(subconjunto.equals(subconj)){
+            return dEstados[i];
+        };
+    }
+}
 const estadoEncabezado = (transiciones, encabezado, arreglo, edo) =>{
     for (let i = 0; i < transiciones.length; i++) {
         const trans = transiciones[i];
@@ -111,11 +133,46 @@ const marcaNovisitados = (transiciones = []) =>{
     });
 }
 
-const construyeAfd = () => {
-
+const construyeAfd = (dEstados = [],encabezados = [], transiciones) => {
+    let encs = encabezados.filter((enc,i,array) => i == array.indexOf(enc));
+    const TABLA = [];
+    for(let k = 0; k < dEstados.length; k++){
+        const dEstadoSub = dEstados[k];
+        const fila = {
+            estado: "",
+            encabezados:[],
+            transiciones: [],
+        };
+        fila.estado = dEstadoSub.nombre;
+        for (let c = 0; c < encs.length; c++) {
+            const encabezado = encs[c];
+            fila.encabezados.push(encabezado);
+            let estados = [];
+            dEstadoSub.subconjuntos.forEach(edo => {
+                estadoEncabezado(transiciones,encabezado,estados,edo);
+            })
+            if(estados.length == 0){
+                fila.transiciones.push("");
+                continue;
+            }
+            marcaNovisitados(transiciones);
+            const subconjuntos = [];
+            estados.forEach((edo,j) => {
+                subconjuntos.push({estado:edo});
+                recorridoEpsilon(edo,transiciones,subconjuntos);
+            });
+            if(subConjuntoExiste(dEstados,subconjuntos)){
+                const nombreEstado = subConjuntoRepetido(dEstados,subconjuntos);
+                fila.transiciones.push(nombreEstado.nombre);
+            }
+        }
+        TABLA.push(fila);
+    }
+    console.log(TABLA);
 }
 
 export function AFD (transiciones = [], encabezados = []) {
     console.log("Creando afd");
- cerraduraEpsilon(transiciones, encabezados);
+    const dEstados = cerraduraEpsilon(transiciones, encabezados);
+    construyeAfd(dEstados,encabezados, transiciones);
 }
