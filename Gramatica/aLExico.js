5511e01cd7849eea08f76bdacc089821dbdbb99a
Raul Ruperto <raul.rupertojs@gmail.com>
2022-04-28 13:41:37 -0500

    Analisis lexico

--- Gramatica/aLExico.js
+++ Gramatica/aLExico.js
@@ -1,22 +1,83 @@
 import "./gramatica";
-const identificador = "[a-z]+";
-const numero = "[0-9]";
-const programa = 
-`
-    read x;
+import {convierteAPosfija} from '../funciones/posfija.js';
+import {afn,encabezados} from '../funciones/afn.js';
+import {AFD} from '../funciones/afd.js'
+const codigo = `
+  read x;
     repeat
-        x:=x+1;
-        write1 x
-    until x < 10
+      x := x + 1;
+      write1 x
+      until x < 10
 `;
+const identificador = "[a-z]+";
+const numero = '[0-9]+';
+const gramatica = new Gramatica();
+const simbolos = gramatica.sTerminales.simbolos;
+const reservadas = gramatica.sTerminales.reservadas;
+
+function onbtenTokens(){
+  let programa = codigo.replace(/[\r\n]/g,"");
+  programa = programa.trim();
+  simbolos.forEach(simbolo => {
+    const regex = new RegExp(`[${simbolo}]`,'g');
+    if(simbolo != "="){
+      if(simbolo == ":="){
+        programa = programa.replace(simbolo, ` ${simbolo} `);
+      }else{
+        programa = programa.replace(regex, ` ${simbolo} `);
+      }
+    }else{
+      programa = programa.replace(regex, `${simbolo} `);
+    }
+  });
+  let prog2 = "";
+  for(let i = 0; i < programa.length; i++){
+    if(programa[i] != ' '){
+      if(programa[i] == '=' && prog2.length > 0){
+        if(prog2[prog2.length -1] != ':' && prog2 != ' ')
+          prog2 += " "
+      }
+      prog2 += programa[i];
+    }else if(programa[i] == ' ' && prog2.length > 0){
+      if(prog2[prog2.length - 1] != ' '){
+        prog2 += " ";
+      }
+    }
+  }
+  let resultado = prog2.trim().split(" ");
+  prog2 = "";
+  resultado.forEach(cadena => {
+    if(cadena.trim() != ""){
+      prog2 += cadena.trim() + " ";
+    }
+  })
+  return prog2.trim().split(" ");
+}
 
 
-function analisis(){
-    const gram = new Gramatica();
-    let listaProg = []
-    listaProg = programa.split(/\s+/).join(" ").trim();
-    console.log(listaProg);
-    gram.sTerminales.simbolos.forEach(simbolo =>{
-        console.log(simbolo);
-    })
+const obtenAfd = (expreg = '') => {
+    const posfija = convierteAPosfija(expreg);
+    const tablaAfn = afn(posfija);
+    const transiciones = tablaAfn[0].transiciones;
+    const encs = encabezados(tablaAfn);
+    const tablaAfd = AFD(transiciones,encs);
+    return tablaAfd.TABLA;
+}
+export function analisisLexico(){
+  const tokens = onbtenTokens();
+  const objeto = {};
+  const afdIdent = obtenAfd(identificador);
+  const afdNum = obtenAfd(numero);
+  tokens.forEach(token => {
+    if(reservadas.lastIndexOf(token) != -1 || simbolos.lastIndexOf(token) != -1){
+      objeto[token] = token
+    }else if(Lexema(token, afdIdent)){
+      objeto[`identificador-${token}`] = token;
+    }else if(Lexema(token, afdNum)){
+      objeto[`Número-${token}`] = token;
+    }else{
+      objeto["Error léxico"] = token;
+    }
+  })
+  return objeto;
 }
